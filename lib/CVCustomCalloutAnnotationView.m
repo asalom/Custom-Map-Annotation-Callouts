54628b4880fabcb5f0453e80c6bfbbd862ab57ed
Fernando Sproviero <fernandospr@gmail.com>
2014-01-31 15:27:36 -0300

    iOS7 look and feel

--- lib/CVCustomCalloutAnnotationView.m
+++ lib/CVCustomCalloutAnnotationView.m
@@ -241,6 +241,9 @@
 - (void)drawRect:(CGRect)rect
 {
 	CGFloat stroke = 1.0;
+    if (floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_6_1) {
+        stroke = 0.3;
+    }
 	CGFloat radius = 7.0;
 	CGMutablePathRef path = CGPathCreateMutable();
 	UIColor *color;
@@ -279,22 +282,33 @@
 	CGPathCloseSubpath(path);
 	
 	//Fill Callout Bubble & Add Shadow
-	color = [[UIColor blackColor] colorWithAlphaComponent:.6];
-	[color setFill];
-	CGContextAddPath(context, path);
-	CGContextSaveGState(context);
-	CGContextSetShadowWithColor(context, CGSizeMake (0, self.yShadowOffset), 6, [UIColor colorWithWhite:0 alpha:.5].CGColor);
-	CGContextFillPath(context);
-	CGContextRestoreGState(context);
-	
+    if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_6_1) {
+        color = [[UIColor blackColor] colorWithAlphaComponent:.6];
+        [color setFill];
+        CGContextAddPath(context, path);
+        CGContextSaveGState(context);
+        CGContextSetShadowWithColor(context, CGSizeMake (0, self.yShadowOffset), 6, [UIColor colorWithWhite:0 alpha:.5].CGColor);
+        CGContextFillPath(context);
+        CGContextRestoreGState(context);
+    } else {
+        color = [UIColor whiteColor];
+        [color setFill];
+        CGContextAddPath(context, path);
+        CGContextFillPath(context);
+    }
+
 	//Stroke Callout Bubble
-	color = [[UIColor darkGrayColor] colorWithAlphaComponent:.9];
+    if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_6_1) {
+        color = [[UIColor darkGrayColor] colorWithAlphaComponent:.9];
+    } else {
+        color = [[UIColor lightGrayColor] colorWithAlphaComponent:.9];
+    }
 	[color setStroke];
 	CGContextSetLineWidth(context, stroke);
 	CGContextSetLineCap(context, kCGLineCapSquare);
 	CGContextAddPath(context, path);
 	CGContextStrokePath(context);
-	
+
 	//Determine Size for Gloss
 	CGRect glossRect = self.bounds;
 	glossRect.size.width = rect.size.width - stroke;
@@ -305,61 +319,65 @@
 	CGFloat glossTopRadius = radius - stroke / 2;
 	CGFloat glossBottomRadius = radius / 1.5;
 	
-	//Create Path For Gloss
-	CGMutablePathRef glossPath = CGPathCreateMutable();
-	CGPathMoveToPoint(glossPath, NULL, glossRect.origin.x, glossRect.origin.y + glossTopRadius);
-	CGPathAddLineToPoint(glossPath, NULL, glossRect.origin.x, glossRect.origin.y + glossRect.size.height - glossBottomRadius);
-	CGPathAddArc(glossPath, NULL, glossRect.origin.x + glossBottomRadius, glossRect.origin.y + glossRect.size.height - glossBottomRadius, 
-				 glossBottomRadius, M_PI, M_PI / 2, 1);
-	CGPathAddLineToPoint(glossPath, NULL, glossRect.origin.x + glossRect.size.width - glossBottomRadius, 
-						 glossRect.origin.y + glossRect.size.height);
-	CGPathAddArc(glossPath, NULL, glossRect.origin.x + glossRect.size.width - glossBottomRadius, 
-				 glossRect.origin.y + glossRect.size.height - glossBottomRadius, glossBottomRadius, M_PI / 2, 0.0f, 1);
-	CGPathAddLineToPoint(glossPath, NULL, glossRect.origin.x + glossRect.size.width, glossRect.origin.y + glossTopRadius);
-	CGPathAddArc(glossPath, NULL, glossRect.origin.x + glossRect.size.width - glossTopRadius, glossRect.origin.y + glossTopRadius, 
-				 glossTopRadius, 0.0f, -M_PI / 2, 1);
-	CGPathAddLineToPoint(glossPath, NULL, glossRect.origin.x + glossTopRadius, glossRect.origin.y);
-	CGPathAddArc(glossPath, NULL, glossRect.origin.x + glossTopRadius, glossRect.origin.y + glossTopRadius, glossTopRadius, 
-				 -M_PI / 2, M_PI, 1);
-	CGPathCloseSubpath(glossPath);
-	
-	//Fill Gloss Path	
-	CGContextAddPath(context, glossPath);
-	CGContextClip(context);
-	CGFloat colors[] =
-	{
-		1, 1, 1, .3,
-		1, 1, 1, .1,
-	};
-	CGFloat locations[] = { 0, 1.0 };
-	CGGradientRef gradient = CGGradientCreateWithColorComponents(space, colors, locations, 2);
-	CGPoint startPoint = glossRect.origin;
-	CGPoint endPoint = CGPointMake(glossRect.origin.x, glossRect.origin.y + glossRect.size.height);
-	CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, 0);
-	
-	//Gradient Stroke Gloss Path	
-	CGContextAddPath(context, glossPath);
-	CGContextSetLineWidth(context, 2);
-	CGContextReplacePathWithStrokedPath(context);
-	CGContextClip(context);
-	CGFloat colors2[] =
-	{
-		1, 1, 1, .3,
-		1, 1, 1, .1,
-		1, 1, 1, .0,
-	};
-	CGFloat locations2[] = { 0, .1, 1.0 };
-	CGGradientRef gradient2 = CGGradientCreateWithColorComponents(space, colors2, locations2, 3);
-	CGPoint startPoint2 = glossRect.origin;
-	CGPoint endPoint2 = CGPointMake(glossRect.origin.x, glossRect.origin.y + glossRect.size.height);
-	CGContextDrawLinearGradient(context, gradient2, startPoint2, endPoint2, 0);
+    if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_6_1) {
+        //Create Path For Gloss
+        CGMutablePathRef glossPath = CGPathCreateMutable();
+        CGPathMoveToPoint(glossPath, NULL, glossRect.origin.x, glossRect.origin.y + glossTopRadius);
+        CGPathAddLineToPoint(glossPath, NULL, glossRect.origin.x, glossRect.origin.y + glossRect.size.height - glossBottomRadius);
+        CGPathAddArc(glossPath, NULL, glossRect.origin.x + glossBottomRadius, glossRect.origin.y + glossRect.size.height - glossBottomRadius, 
+                     glossBottomRadius, M_PI, M_PI / 2, 1);
+        CGPathAddLineToPoint(glossPath, NULL, glossRect.origin.x + glossRect.size.width - glossBottomRadius, 
+                             glossRect.origin.y + glossRect.size.height);
+        CGPathAddArc(glossPath, NULL, glossRect.origin.x + glossRect.size.width - glossBottomRadius, 
+                     glossRect.origin.y + glossRect.size.height - glossBottomRadius, glossBottomRadius, M_PI / 2, 0.0f, 1);
+        CGPathAddLineToPoint(glossPath, NULL, glossRect.origin.x + glossRect.size.width, glossRect.origin.y + glossTopRadius);
+        CGPathAddArc(glossPath, NULL, glossRect.origin.x + glossRect.size.width - glossTopRadius, glossRect.origin.y + glossTopRadius, 
+                     glossTopRadius, 0.0f, -M_PI / 2, 1);
+        CGPathAddLineToPoint(glossPath, NULL, glossRect.origin.x + glossTopRadius, glossRect.origin.y);
+        CGPathAddArc(glossPath, NULL, glossRect.origin.x + glossTopRadius, glossRect.origin.y + glossTopRadius, glossTopRadius, 
+                     -M_PI / 2, M_PI, 1);
+        CGPathCloseSubpath(glossPath);
+        
+        //Fill Gloss Path	
+        CGContextAddPath(context, glossPath);
+        CGContextClip(context);
+        CGFloat colors[] =
+        {
+            1, 1, 1, .3,
+            1, 1, 1, .1,
+        };
+        CGFloat locations[] = { 0, 1.0 };
+        CGGradientRef gradient = CGGradientCreateWithColorComponents(space, colors, locations, 2);
+        CGPoint startPoint = glossRect.origin;
+        CGPoint endPoint = CGPointMake(glossRect.origin.x, glossRect.origin.y + glossRect.size.height);
+        CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, 0);
+        
+        //Gradient Stroke Gloss Path	
+        CGContextAddPath(context, glossPath);
+        CGContextSetLineWidth(context, 2);
+        CGContextReplacePathWithStrokedPath(context);
+        CGContextClip(context);
+        CGFloat colors2[] =
+        {
+            1, 1, 1, .3,
+            1, 1, 1, .1,
+            1, 1, 1, .0,
+        };
+        CGFloat locations2[] = { 0, .1, 1.0 };
+        CGGradientRef gradient2 = CGGradientCreateWithColorComponents(space, colors2, locations2, 3);
+        CGPoint startPoint2 = glossRect.origin;
+        CGPoint endPoint2 = CGPointMake(glossRect.origin.x, glossRect.origin.y + glossRect.size.height);
+        CGContextDrawLinearGradient(context, gradient2, startPoint2, endPoint2, 0);
+
+        //Cleanup
+        CGPathRelease(glossPath);
+        CGGradientRelease(gradient);
+        CGGradientRelease(gradient2);
+    }
 	
 	//Cleanup
 	CGPathRelease(path);
-	CGPathRelease(glossPath);
 	CGColorSpaceRelease(space);
-	CGGradientRelease(gradient);
-	CGGradientRelease(gradient2);
 }
 
 @end
